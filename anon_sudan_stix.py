e40e62ec31b5e86b2558895a3ca41b5f7a526647
Wesley <pl450b@localhost.localdomain>
2023-07-07 10:04:56 -0400

    added notes

--- anon_sudan_stix.py
+++ anon_sudan_stix.py
@@ -1,8 +1,34 @@
 from stix2 import *
 
+# ----- Threat Actors -----
+anonSu = ThreatActor(name="Anoymous Sudan",
+                    description="Hacktivist group")
+
+killnet = ThreatActor(name="KILLNET",
+                      description="Russia-aligned hacktivist group, specialized in DDoS attacks")
+
+revil = ThreatActor(name="REvil",
+                      description="Russian based RaaS operation. Would post the contents of attack \
+                      on their \"Happy Blog\" site. Russian Federal Security Service says they dismantled \
+                        the group and charged several of it's members")
+
+
+# ----- Campaigns -----
+microsoft = Campaign(name="Microsoft Office Attack",
+                     description="Anonymous Sudan attacks microsoft servers with a DDoS attack",
+                     aliases="Storm-1359")
+# russia_ukraine = Campaign()
+
+# ----- Attack Patterns -----
+
+# ----- Object Lists -----
+threat_list = [anonSu, killnet, revil]
+campaign_list = [microsoft]
+pattern_list = []
+
 # Creates a full bundle and prints out the JSON content
 # Pass each SDO/SRO to the `Bundle()` contructor
-bundle = Bundle()
+bundle = Bundle(threat_list, campaign_list, pattern_list)
 print(bundle.serialize(pretty=True))
 
 # Send to a local json file to give to online viewer
